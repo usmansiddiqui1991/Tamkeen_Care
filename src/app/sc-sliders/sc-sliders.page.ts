3adfa14eafcab751db2b12499d58af33e7789c5d
Tayyabah <tghulam@tamkeen-ksa.com>
2020-01-16 12:06:05 +0300

    t

--- src/app/sc-sliders/sc-sliders.page.ts
+++ src/app/sc-sliders/sc-sliders.page.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-sc-sliders',
   templateUrl: './sc-sliders.page.html',
   styleUrls: ['./sc-sliders.page.scss'],
-  
 })
 export class ScSlidersPage implements OnInit {
 
