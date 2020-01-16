6260d7e00e6baf951f3afba86cbb4015087186c0
Muhammad Usman Siddiqui <usman@tmakeen-ksa.com>
2020-01-16 12:03:00 +0300

    U_sliders

--- src/app/sc-sliders/sc-sliders.page.ts
+++ src/app/sc-sliders/sc-sliders.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-sc-sliders',
   templateUrl: './sc-sliders.page.html',
   styleUrls: ['./sc-sliders.page.scss'],
+  
 })
 export class ScSlidersPage implements OnInit {
 
