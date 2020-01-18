6ad0c1703c411b89ffae161f30dd6fd6cef9ff9e
Muhammad Usman Siddiqui <usman@tmakeen-ksa.com>
2020-01-18 18:16:17 +0300

    Yes

---
+++ src/app/sc-otp-code/sc-otp-code.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sc-otp-code',
+  templateUrl: './sc-otp-code.page.html',
+  styleUrls: ['./sc-otp-code.page.scss'],
+})
+export class ScOtpCodePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
